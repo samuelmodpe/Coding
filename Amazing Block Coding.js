39016311034f8727ea8c7425f9e39bac82460f59
samuelmodpe <androooidgameplays@gmail.com>
2014-10-25 11:38:10 -0400

    Create Amazing Block Coding.js

---
+++ Amazing Block Coding.js
@@ -0,0 +1,10 @@
+Coding
+======
+
+Coding MinecraftPE Mod's
+
+ Block.defineBlock(93,"Trampoline",[["gold_block",0],["glass",0],["stone",0],["sand",0],["diamond_block",0],["bedrock",0]],50,1,0);
+ Block.setDestroyTime(93,2);                    //1                    2                   3                 4                   5                                6
+  Block.setLightLevel(93,25)
+   Block.setShape(93, 2/6, 0, 2/6, 4/6, 1/2, 4/6);
+   
